50d291b6cc261bec73e0a4cb27b3a0e2eded0128
Garrett Hoofman <gambitsunob@gmail.com>
2018-01-12 17:30:21 -0500

    cleanup codes

--- Web/Controllers/CodeController.cs
+++ Web/Controllers/CodeController.cs
@@ -56,6 +56,11 @@ namespace Fyo.Controllers
                 return new NotFoundObjectResult(false);
             }
 
+            var expiredCodes = _codeService.GetAll().Where(x => x.CreatedDate < DateTime.Now.AddMinutes(-10));
+            foreach(var c in expiredCodes) {
+                _codeService.Delete(c);
+            }
+
             var digits = "";
             var r = new Random();
             for(var i = 0; i < 7; i++) {
